9ddc49b9b7c1ff13a1d190ad982b3a8281e6c6c1
Lugo Chavez Paola <>
2023-04-14 19:14:01 -0500

    Practica 8 proyecto de exàmen

---
+++ src/g4s21p8plc/main.java
@@ -0,0 +1,14 @@
+
+package g4s21p8plc;
+
+import PacExamen.JFExamen;
+
+public class main {
+
+   
+    public static void main(String[] args) {
+       
+     new JFExamen().setVisible(true);
+        
+    }
+}
